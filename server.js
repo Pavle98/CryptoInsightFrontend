7fe7d5fb4c5864b004e639edbefeb078a3950dd9
Pavle98 <pavlevukovic98@gmail.com>
2023-08-13 21:27:26 +0200

    #

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const history = require('connect-history-api-fallback');
+
+const app = express();
+
+// Use the history API fallback middleware to handle potential 404 responses.
+// This is especially useful if you are using Vue Router in 'history' mode.
+app.use(history());
+
+// Serve static assets from the 'dist' directory
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Handle any other routes to the root of the app
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
